d34361feab0f6ed45cdf4712ec76e5aeffe60f0d
TheSabaru <83044384+TheSabaru@users.noreply.github.com>
2022-08-28 15:45:24 -0500

    Add files via upload

---
+++ servalfix/__init__.py
@@ -0,0 +1,20 @@
+__title__ = 'serval.fix'
+__author__ = 'Capuchino'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022-202# Capuchino'
+__version__ = '2.3.4.11'
+
+from .acm import ACM
+from .client import Client
+from .sub_client import SubClient
+from .lib.util import device, exceptions, helpers, objects, headers
+from .asyncfix import acm, client, sub_client, socket
+from .socket import Callbacks, SocketHandler
+from requests import get
+from json import loads
+
+__newest__ = loads(get("https://pypi.org/pypi/serval.fix/json").text)["info"]["version"]
+
+if __version__ != __newest__:
+    print(f"New version of {__title__} available: {__newest__} (Using {__version__})")
+    print("Visit our discord - https://discord.gg/7NmhxQBmET")
---
+++ servalfix/asyncfix/__init__.py
@@ -0,0 +1,10 @@
+__title__ = 'serval.fix-async'
+__author__ = 'Capuchino'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022-202# Capuchino'
+
+from .acm import ACM
+from .client import Client
+from .sub_client import SubClient
+from .socket import Callbacks, SocketHandler
+
