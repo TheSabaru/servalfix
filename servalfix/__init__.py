455a04d3255b449c426ffe4ed3f9151913962f82
TheSabaru <83044384+TheSabaru@users.noreply.github.com>
2022-08-28 16:11:04 -0500

    Add files via upload

--- servalfix/__init__.py
+++ servalfix/__init__.py
@@ -1,4 +1,4 @@
-__title__ = 'serval.fix'
+__title__ = 'servalfix'
 __author__ = 'Capuchino'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2022-202# Capuchino'
@@ -13,7 +13,7 @@ from .socket import Callbacks, SocketHandler
 from requests import get
 from json import loads
 
-__newest__ = loads(get("https://pypi.org/pypi/serval.fix/json").text)["info"]["version"]
+__newest__ = loads(get("https://pypi.org/pypi/amino.fix/json").text)["info"]["version"]
 
 if __version__ != __newest__:
     print(f"New version of {__title__} available: {__newest__} (Using {__version__})")
