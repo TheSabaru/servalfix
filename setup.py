c2782e7ab40436eb9553dde3f240d51ca0007b99
TheSabaru <83044384+TheSabaru@users.noreply.github.com>
2022-08-28 16:13:29 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -16,19 +16,20 @@ with open("README.md", "r") as stream:
     long_description = stream.read()
 
 setup(
-    name="serval.fix",
+    name="servalfix",
     license='MIT',
     author="Capuchino",
-    version="2.3.4.11",
+    version="2.3.4.11.1",
     author_email="chocolabienid@gmail.com",
     description="Library for AminoApps. Discord - https://discord.gg/7NmhxQBmET",
-    url="https://github.com/TheSabaru/serval.fix",
+    url="https://github.com/TheSabaru/servalfix",
     packages=find_packages(),
     long_description=long_description,
     install_requires=requirements,
     keywords=[
         'aminoapps',
-        'serval.fix',
+        'servalfix',
+        'servalfix',
         'serval',
         'serval-bot',
         'narvii',
@@ -37,7 +38,7 @@ setup(
         'python',
         'python3',
         'python3.x',
-        'serval.fix.py',
+        'servalfix.py',
         'serval-amino',
         'capuchino'
     ],
